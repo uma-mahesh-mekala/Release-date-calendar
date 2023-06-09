1311228e88cdb899daa786960b1450f6c4fa8976
Mahesh <maheshuma154@gmail.com>
2023-06-09 21:24:36 +0530

    updated

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 const port = 3000;
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Mahesh@2001',
-//     database:'releases_db'
-// })
-// connection.connect(function (err) { 
-//     if (err) throw err;
-//     console.log("connection is established");
-
-// }
-// )
 app.listen(port, () => { 
     console.log(`server stared at ${port}`);
 })
@@ -27,10 +14,6 @@ app.get("/", (req, res)=>{
 app.post("/", (req, res) => { 
     var postData = req.body;
     delete postData.submit;
-    // connection.query('INSERT INTO releases SET?', postData, function (err, results) { 
-    //     if (err) throw err;
-    //     console.log("last record inserted at:", results.insertID);
-    // })
     var artistName = req.body.artistName;
     var songTitle = req.body.songTitle;
     var date = (req.body.date).split('-').reverse().join('-h');
